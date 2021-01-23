b919e9cf35a222a363dd220a34393e74993f6245
R. van Twisk <github@rvt.dds.nl>
2021-01-23 01:08:38 +0100

    Update scriptcontext.hpp
    Better logic on deciding when to turn on the pump

--- src/scriptcontext.hpp
+++ src/scriptcontext.hpp
@@ -8,7 +8,6 @@ typedef PlainTextContext<512> PlainTextContext512;
 class ScriptContext : public PlainTextContext512 {
 public:
     bool m_pump;
-    uint16_t m_lastValue;
 
     uint16_t m_maxThreshold;
     uint16_t m_minThreshold;
@@ -16,40 +15,27 @@ public:
     bool m_watering;
     ScriptContext(const char* script) : 
         PlainTextContext512{script}, 
-        m_pump(false), 
-        m_lastValue(0),
+        m_pump(false),
         m_maxThreshold(0),
         m_minThreshold(0),
-        m_currentValue(1024),
-        m_watering(true)
+        m_currentValue(1024)
     {
     }
 
     bool decide() {
         // Protection against loose connections
         if (m_currentValue > 1020) {
-            return false;
+            m_pump=false;
         }
         // Set watering mode
-        if ( m_currentValue >= m_maxThreshold) {
-            m_watering=true;
-            return true;
+        else if ( m_currentValue >= m_maxThreshold) {
+            m_pump=true;
         }
         // remove watering mode
-        if ( m_currentValue <= m_maxThreshold && m_watering) {
-            m_watering=false;
-            return false;
+        else if ( m_currentValue <= m_minThreshold) {
+            m_pump=false;
         }
-        // We come from > max to > max, we should add more water
-        if ( m_currentValue >= m_maxThreshold && m_lastValue >= m_maxThreshold) {
-            m_lastValue = m_currentValue;
-            return true;
-        }
-
-        // We come from > max to < min, we store last value
-        if ( m_currentValue <= m_minThreshold && m_lastValue >= m_maxThreshold) {
-            m_lastValue = m_currentValue;
-        }
-        return false;
+        
+        return m_pump;
     }
 };
